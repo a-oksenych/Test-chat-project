f5ef2cea84e9b1cd1416e926d7def5f13ef09f2e
m-labazov <maksym.labazov@gmail.com>
2012-07-26 18:26:21 +0300

    Added own broker creation Subscription was added

--- test-chat/src/main/java/com/epam/chat/camel/JMSComponentFactory.java
+++ test-chat/src/main/java/com/epam/chat/camel/JMSComponentFactory.java
@@ -2,18 +2,14 @@ package com.epam.chat.camel;
 
 import com.epam.chat.IConnectionFactory;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.Component;
-import org.apache.camel.component.jms.JmsComponent;
-
-import javax.jms.ConnectionFactory;
 
 public class JMSComponentFactory implements IConnectionFactory {
 
     @Override
     public Component createComponent(String url) {
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
-        Component result = JmsComponent.jmsComponent(connectionFactory);
+        Component result = ActiveMQComponent.activeMQComponent(url);
         return result;
     }
 
