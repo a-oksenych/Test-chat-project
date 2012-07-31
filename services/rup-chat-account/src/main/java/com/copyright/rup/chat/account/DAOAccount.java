74bb909fed843b06c31c99682a014f5552e05df3
Andriy Oksenych <Andriy_Oksenych@epam.com>
2012-07-31 18:37:38 +0300

    update MyBatis Mapping

--- services/rup-chat-account/src/main/java/com/copyright/rup/chat/account/DAOAccount.java
+++ services/rup-chat-account/src/main/java/com/copyright/rup/chat/account/DAOAccount.java
@@ -1,57 +1,38 @@
 package com.copyright.rup.chat.account;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.log4j.Logger;
 
 import com.copyright.rup.chat.common.Account;
 import com.copyright.rup.chat.common.MyBatisConnectionFactory;
+import com.copyright.rup.chat.common.Room;
 
 /**
  * @author Andriy Oksenych
  * 
  */
-public class DAOAccount implements IDAOAccount {
+public class DAOAccount implements IDAOAccount, InfoMapper {
 	Logger logger = Logger.getLogger(DAOAccountStub.class);
-    
-    public interface InfoMapper {
-    	final String CREATE_ACCOUNT = "INSERT INTO account(name, email, password) VALUES (#{name}, #{email}, #{password})";
-    	final String GET_ACCOUNT = "SELECT * FROM ACCOUNT WHERE id = #{idAccount}";
-    	final String UPDATE_ACCOUNT = "UPDATE account SET name = #{name}, email = #{email}, password = #{password} WHERE id = #{id}";
-    	final String DELETE_ACCOUNT = "DELETE FROM account WHERE id = #{id}";
-    	
-    	@INSERT(CREATE_ACCOUNT)
-    	public void createAccount(Account account) throws Exception;
-    	
-    	@SELECT(GET_ACCOUNT)
-    	public Account getAccount(int idAccount) throws Exception;
-    	
-    	@UPDATE(UPDATE_ACCOUNT)
-    	public void updateAccount(Account account) throws Exception;
-    	
-    	@DELETE(DELETE_ACCOUNT)
-    	public void deleteAccount(int id) throws Exception;
-    }
 
     @Override
     public Account createAccount(Account account) {
+    	Account createdAccount = null;
         SqlSession session = getSession();
 		try {
 			InfoMapper mapper = session.getMapper(InfoMapper.class);
-			mapper.insertInformation(account);
+			createdAccount = mapper.insertInformation(account);
 		} catch (Exception e) {
 			logger.error("Can't create an account");
 		} finally {
 			session.close();
 		}
         
-        return account;
+        return createdAccount;
     }
 
     @Override
@@ -61,7 +42,7 @@ public class DAOAccount implements IDAOAccount {
 			InfoMapper mapper = session.getMapper(InfoMapper.class);
 			return mapper.getAccount(idAccount);
 		} catch (Exception e) {
-			logger.error("Can't create an account");
+			logger.error("Can't select an account");
 		} finally {
 			session.close();
 		}
@@ -74,23 +55,39 @@ public class DAOAccount implements IDAOAccount {
 			InfoMapper mapper = session.getMapper(InfoMapper.class);
 			mapper.updateAccount(account);
 		} catch (Exception e) {
-			logger.error("Can't create an account");
+			logger.error("Can't update an account");
 		} finally {
 			session.close();
 		}
     }
 
     @Override
-    public void deleteAccount(int id) {
+    public void deleteAccount(int idAccount) {
     	SqlSession session = getSession();
 		try {
 			InfoMapper mapper = session.getMapper(InfoMapper.class);
 			mapper.deleteAccount(id);
 		} catch (Exception e) {
-			logger.error("Can't create an account");
+			logger.error("Can't delete an account");
 		} finally {
 			session.close();
 		}
+    }
+    
+    @Override
+    public List<Room> getRoomsForAccount(int idAccount) {
+    	List<Room> rooms = null;
+    	SqlSession session = getSession();
+		try {
+			InfoMapper mapper = session.getMapper(InfoMapper.class);
+			rooms = mapper.getRoomsForAccount(idAccount);
+		} catch (Exception e) {
+			logger.error("Can't delete an account");
+		} finally {
+			session.close();
+		}
+		
+		return rooms;
     }
     
     private SqlSession getSession() {
